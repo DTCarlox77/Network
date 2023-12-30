3f6a24b78f8216d762e1b445aae52b1d5d081803
DTCarlox77 <carlosadrianespinosaluna147@gmail.com>
2023-12-30 01:35:17 -0600

    Plantillas terminadas

--- network/views.py
+++ network/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 def main(request):
     return render(request, 'index.html')
 
+@csrf_protect
 def login(request):
+    
+    if request.method == 'POST':
+        # Proceso de los datos del formulario.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+    
     return render(request, 'login.html')
 
 def sign_in(request):
